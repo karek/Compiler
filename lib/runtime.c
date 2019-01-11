003462e0bb57aad6a5fc0473e54e93c8b5445819
Kar <Krzysztof.Kiljan@student.uw.edu.pl>
2019-01-11 14:59:31 +0100

    Finally compiling ?

--- lib/runtime.c
+++ lib/runtime.c
@@ -1,45 +1,65 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
 
+typedef struct _iobuf {
+    char* _ptr;
+    int _cnt;
+    char* _base;
+    int _flag;
+    int _file;
+    int _charbuf;
+    int _bufsiz;
+    char* _tmpfname;
+} FILE;
+extern FILE* stdin;
+
+extern int printf(const char* fmt, ...);
+extern int scanf(const char* fmt, ...);
+extern void exit(int code);
+extern int getline(char** lineptr, unsigned int* n, FILE* stream);
+extern unsigned int strlen(const char* str);
+void* malloc(unsigned int size);
+void* memcpy(void* destination, const void* source, unsigned int num);
+#define NULL ((void*)0)
 
 void printInt(int x) {
-	printf("%d\n", x);
+    printf("%d\n", x);
 }
 
 void printString(const char* s) {
-	printf("%s\n",s);
+    printf("%s\n", s);
 }
 
 void error() {
     printf("runtime error\n");
-	exit(1);
+    exit(1);
 }
 
 int readInt() {
-	int res;
+    int res;
     scanf("%d\n", &res);
-	return res;
+    return res;
 }
 
 char* readString() {
-	char* line = NULL;
-    size_t siz = 0;
+    char* line = NULL;
+    unsigned int siz = 0;
     getline(&line, &siz, stdin);
 
     int len = strlen(line);
-	line[len - 1] = '\0';
-	return line;
+    line[len - 1] = '\0';
+    return line;
 }
 
 char* __concatStrings(const char* a, const char* b) {
-	int len1 = strlen(a);
+    int len1 = strlen(a);
     int len2 = strlen(b);
     int newLen = len1 + len2 + 1;
-    char* res = (char*) malloc(sizeof(char) * newLen);
-    
+    char* res = (char*)malloc(sizeof(char) * newLen);
+
     memcpy(res, a, len1);
     memcpy(res + len1, b, len2);
 
-	return res;
+    return res;
 }
